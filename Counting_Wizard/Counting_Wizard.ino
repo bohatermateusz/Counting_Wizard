0760ae7174d7bfd4ef9071d62b8eebed50dcc26c
bohatermateusz <bohatermateusz@gmail.com>
2022-01-09 14:27:45 +0100

    Added WebSocketClient

--- Counting_Wizard/Counting_Wizard.ino
+++ Counting_Wizard/Counting_Wizard.ino
@@ -12,6 +12,12 @@
 
 static std::vector<AsyncClient*> clients; // a list to hold all clients
 
+//AsyncWebSocketClient
+#include <WebSocketsClient.h>
+#include <Hash.h>
+WebSocketsClient webSocket;
+#define USE_SERIAL Serial
+
 #include "WiFiManager.h"
 WiFiManager wifiManager;
 
@@ -82,6 +88,27 @@ void setup()
     Wire.begin();
     Serial.begin(115200);
 
+      for(uint8_t t = 4; t > 0; t--) {
+    USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+    USE_SERIAL.flush();
+    delay(1000);
+      }
+
+      // server address, port and URL
+    webSocket.begin("192.168.0.150", 80, "/ws");
+
+      // event handler
+    webSocket.onEvent(webSocketEvent);
+      // try ever 5000 again if connection has failed
+    webSocket.setReconnectInterval(5000);
+
+      // start heartbeat (optional)
+      // ping server every 15000 ms
+      // expect pong from server within 3000 ms
+      // consider connection disconnected if pong is not received 2 times
+    webSocket.enableHeartbeat(15000, 3000, 2);
+    
+    
     //pinmode for button purpose
     pinMode(inPin, INPUT);
 
@@ -187,6 +214,7 @@ String getLimit()
 
 void loop()
 {
+  webSocket.loop();
   DNS.processNextRequest();
     // check button status
     val = digitalRead(inPin); // read input value
@@ -646,4 +674,42 @@ static void handleNewClient(void* arg, AsyncClient* client) {
   client->onError(&handleError, NULL);
   client->onDisconnect(&handleDisconnect, NULL);
   client->onTimeout(&handleTimeOut, NULL);
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+
+  switch(type) {
+    case WStype_DISCONNECTED:
+      USE_SERIAL.printf("[WSc] Disconnected!\n");
+      break;
+    case WStype_CONNECTED: {
+      USE_SERIAL.printf("[WSc] Connected to url: %s\n", payload);
+
+      // send message to server when Connected
+      webSocket.sendTXT("Connected");
+    }
+      break;
+    case WStype_TEXT:
+      USE_SERIAL.printf("[WSc] get text: %s\n", payload);
+
+      // send message to server
+      // webSocket.sendTXT("message here");
+      break;
+    case WStype_BIN:
+      USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
+      hexdump(payload, length);
+
+      // send data to server
+      // webSocket.sendBIN(payload, length);
+      break;
+        case WStype_PING:
+            // pong will be send automatically
+            USE_SERIAL.printf("[WSc] get ping\n");
+            break;
+        case WStype_PONG:
+            // answer to a ping we send
+            USE_SERIAL.printf("[WSc] get pong\n");
+            break;
+    }
+
 }
