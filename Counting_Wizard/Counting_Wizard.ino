468780ffe973c1d86d9fb416fe2c47938f9247cc
bohatermateusz <bohatermateusz@gmail.com>
2022-05-04 20:41:33 +0200

    minor optimization

--- Counting_Wizard/Counting_Wizard.ino
+++ Counting_Wizard/Counting_Wizard.ino
@@ -274,7 +274,7 @@ void setup()
   esp_now_register_send_cb(OnDataSent);
   esp_now_register_recv_cb(OnDataRecv);
 
-  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
+  //esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
 
   // Start server
   server.begin();
